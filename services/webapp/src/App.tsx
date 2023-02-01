9d5e1e9e6169c3cd7df03484e9a99e24e7bf5b21
Masudul Haque <saadixl@outlook.com>
2023-02-01 20:20:24 +0800

    Pass api domain from env to webapp

--- services/webapp/src/App.tsx
+++ services/webapp/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-const NEWS_API_URL = 'http://news-api:5001/get-news/';
+const NEWS_API_URL = `${process.env.NEWS_API_DOMAIN}/get-news/`;
 
 function App() {
   const [news, setNews] = useState();
