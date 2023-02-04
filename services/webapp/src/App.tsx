b10fdda250dd68286edd682aceae1b428cd7dbd2
Masudul Haque <saadixl@outlook.com>
2023-02-05 01:40:25 +0800

    Revert "Revamp loading text"
    This reverts commit 5bf0d068919128522670e82a06710943534923a1.

--- services/webapp/src/App.tsx
+++ services/webapp/src/App.tsx
@@ -61,7 +61,7 @@ function App() {
 
   function renderNews(news: any) {
     if (!news) {
-      return <p className="loading-text">news loading..</p>;
+      return <p>news loading..</p>;
     }
     const comps: any = [];
     Object.keys(news).forEach((topic) => {
