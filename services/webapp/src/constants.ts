7607bf197ab473f36662cfd2e5a26776673c768f
Masudul Haque <saadixl@outlook.com>
2023-10-18 00:02:09 +0800

    Get domain name from frontend for now

--- services/webapp/src/constants.ts
+++ services/webapp/src/constants.ts
@@ -1,4 +1,5 @@
-const newsApiDomain = "http://mh7.pw:5001";
+const newsApiDomain = `http://${window.location.hostname}:5001`;
+
 export const GET_NEWS_API_URL = `${newsApiDomain}/get-news/`;
 export const GET_TOPICS_API_URL = `${newsApiDomain}/get-topics/`;
 export const SET_TOPICS_API_URL = `${newsApiDomain}/set-topics/`;
